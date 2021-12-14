e30b6442cb999c8b315001e1cd0f43500d648470
Ygnacio Durán <yduran@grupolanka.com>
2021-12-14 16:08:19 -0400

    Add project files.

---
+++ noSleep/noSleep.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace noSleep
+{
+    partial class noSleep
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.WakeUpTimer = new System.Windows.Forms.Timer(this.components);
+            this.ScrollLockStatus = new System.Windows.Forms.CheckBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInterval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // WakeUpTimer
+            // 
+            this.WakeUpTimer.Enabled = true;
+            this.WakeUpTimer.Interval = 60000;
+            this.WakeUpTimer.Tick += new System.EventHandler(this.WakeUpTimer_Tick);
+            // 
+            // ScrollLockStatus
+            // 
+            this.ScrollLockStatus.AutoSize = true;
+            this.ScrollLockStatus.Enabled = false;
+            this.ScrollLockStatus.Location = new System.Drawing.Point(12, 6);
+            this.ScrollLockStatus.Name = "ScrollLockStatus";
+            this.ScrollLockStatus.Size = new System.Drawing.Size(79, 17);
+            this.ScrollLockStatus.TabIndex = 0;
+            this.ScrollLockStatus.Text = "Scroll Lock";
+            this.ScrollLockStatus.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(135, 6);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(135, 35);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Timer (min)";
+            // 
+            // tbInterval
+            // 
+            this.tbInterval.Location = new System.Drawing.Point(82, 33);
+            this.tbInterval.Name = "tbInterval";
+            this.tbInterval.Size = new System.Drawing.Size(30, 20);
+            this.tbInterval.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "noSleep";
+            this.notifyIcon.Visible = true;
+            // 
+            // noSleep
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 84);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbInterval);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.ScrollLockStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "noSleep";
+            this.Text = "noSleep";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.noSleep_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer WakeUpTimer;
+        private System.Windows.Forms.CheckBox ScrollLockStatus;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInterval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ noSleep/noSleep.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace noSleep
+{
+    public partial class noSleep : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        private const uint KEYEVENTF_KEYUP = 0x2;
+
+        private const int TIME_FACTOR = 1000 * 60;
+        private const int DEFAULT_TIME = 10;
+
+        //[DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        //public static extern short GetKeyState(int keyCode);
+
+        public noSleep()
+        {
+            InitializeComponent();
+            tbInterval.Text = DEFAULT_TIME.ToString();
+            WakeUpTimer.Enabled = false;
+            label2.Text = "Timer Stopped";
+        }
+
+        private async void WakeUpTimer_Tick(object sender, EventArgs e)
+        {
+            int i = 0;
+            // Activar / desactivar la tecla scroll lock con un intervalo de 1/2 sec
+            do
+            {
+                keybd_event((byte)Keys.Scroll, 0, 0, 0);
+                keybd_event((byte)Keys.Scroll, 0, KEYEVENTF_KEYUP, 0);
+                ScrollLockStatus.Checked = Control.IsKeyLocked(Keys.Scroll);
+                i++;
+                await PutTaskDelay(500);
+            } while (i < 2);
+        }
+
+        private void noSleep_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            WakeUpTimer.Stop();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(tbInterval.Text, out int number))
+            {
+                WakeUpTimer.Interval = number * TIME_FACTOR;
+            }
+            else
+            {
+                WakeUpTimer.Interval = DEFAULT_TIME * TIME_FACTOR;
+                tbInterval.Text = DEFAULT_TIME.ToString();
+            }
+
+            WakeUpTimer.Start();
+            label2.Text = "Timer Started";
+            tbInterval.Enabled = false;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            WakeUpTimer.Stop();
+            label2.Text = "Timer Stopped";
+            tbInterval.Enabled = true;
+        }
+
+        private async Task PutTaskDelay(int ms)
+        {
+            await Task.Delay(ms);
+        }
+    }
+}
